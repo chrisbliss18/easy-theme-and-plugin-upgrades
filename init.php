64c2d67dd0d23ce27c73f2e06390f08e43ccd2a8
Chris Jean <gaarai@gaarai.com>
2016-07-19 10:47:12 -0500

    Updating plugin header.

--- init.php
+++ init.php
@@ -2,11 +2,14 @@
 
 /*
 Plugin Name: Easy Theme and Plugin Upgrades
-Plugin URI: http://wordpress.org/extend/plugins/easy-theme-and-plugin-upgrades/
-Description: This plugin allows for installed themes to be upgraded by using the Appearance > Add New Themes > Upload feature of WordPress. Without this plugin, themes can only be installed using this method, requiring you to first delete the theme before installing the newer version. Now features the same easy upgrading for plugins via the Plugins > Add New > Upload page.
-Author: Chris Jean
-Version: 1.0.6
-Author URI: http://ithemes.com/
+Plugin URI:  http://wordpress.org/extend/plugins/easy-theme-and-plugin-upgrades/
+Description: Upgrade themes and plugins using a zip file without having to remove them first.
+Author:      Chris Jean
+Author URI:  https://chrisjean.com/
+Version:     1.0.6-dev
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: easy-theme-and-plugin-upgrades
 */
 
 
