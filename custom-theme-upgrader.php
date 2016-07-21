92fb9fc55c09ee9024648fa42d951dc4ed2bdec6
Chris Jean <gaarai@gaarai.com>
2016-07-21 11:31:10 -0500

    Rewrite to use subclasses rather than hooks to handle upgrades.

--- custom-theme-upgrader.php
+++ custom-theme-upgrader.php
@@ -1,13 +1,176 @@
 <?php
 
 class CAJ_ETPU_Theme_Upgrader extends Theme_Upgrader {
-	public function run( $options ) {
-		$options['clear_destination'] = true;
+	public function install_package( $args = array() ) {
+		global $wp_filesystem;
 
-		return parent::run( $options );
+
+		if ( empty( $args['source'] ) || empty( $args['destination'] ) ) {
+			// Only run if the arguments we need are present.
+			return parent::install_package( $args );
+		}
+
+
+		$source_files = array_keys( $wp_filesystem->dirlist( $args['source'] ) );
+		$remote_destination = $wp_filesystem->find_folder( $args['destination'] );
+
+		//Locate which directory to copy to the new folder, This is based on the actual folder holding the files.
+		if ( 1 == count( $source_files ) && $wp_filesystem->is_dir( trailingslashit( $args['source'] ) . $source_files[0] . '/' ) ) { //Only one folder? Then we want its contents.
+			$destination = trailingslashit( $remote_destination ) . trailingslashit( $source_files[0] );
+		} else if ( count( $source_files ) == 0 ) {
+			// Looks like an empty zip, we'll let the default code handle this.
+			return parent::install_package( $args );
+		} else { // It's only a single file, the upgrader will use the folder name of this file as the destination folder. Folder name is based on zip filename.
+			$destination = trailingslashit( $remote_destination ) . trailingslashit( basename( $args['source'] ) );
+		}
+
+
+		if ( is_dir( $destination ) && file_exists( "$destination/style.css" ) ) {
+			// This is an upgrade, clear the destination.
+			$args['clear_destination'] = true;
+
+			// Switch template strings to use upgrade terminology rather than install terminology
+			$this->upgrade_strings();
+
+			// Replace default remove_old string to make the messages more meaningful.
+			$this->strings['installing_package'] = __( 'Upgrading the theme&#8230;', 'easy-theme-and-plugin-upgrades' );
+			$this->strings['remove_old'] = __( 'Backing up the old version of the theme&#8230;', 'easy-theme-and-plugin-upgrades' );
+		}
+
+		return parent::install_package( $args );
 	}
 
-	public function clear_destination( $remote_destination ) {
-		error_log( 'Remote Destination: ' . $remote_destination );
+	public function clear_destination( $destination ) {
+		if ( ! is_dir( $destination ) || ! file_exists( "$destination/style.css" ) ) {
+			// This is an installation not an upgrade.
+			return parent::clear_destination( $destination );
+		}
+
+
+		$backup_url = $this->create_backup( $destination );
+
+		if ( ! is_wp_error( $backup_url ) ) {
+			/* translators: 1: theme zip URL */
+			$this->skin->feedback( sprintf( __( 'A backup zip file of the old theme version can be downloaded <a href="%1$s">here</a>.', 'easy-theme-and-plugin-upgrades' ), $backup_url ) );
+
+			// Restore default strings and display the original remove_old message.
+			$this->upgrade_strings();
+			$this->skin->feedback( 'remove_old' );
+
+			return parent::clear_destination( $destination );
+		}
+
+
+		$this->skin->error( $backup_url );
+		$this->skin->feedback( __( 'Moving the old version of the theme to a new directory&#8230;', 'easy-theme-and-plugin-upgrades' ) );
+
+
+		$headers = array(
+			'version' => 'Version',
+		);
+		$data = get_file_data( "$destination/style.css", $headers );
+
+		$new_name = basename( $destination ) . "-{$data['version']}";
+		$directory = dirname( $destination );
+
+		for ( $x = 0; $x < 20; $x++ ) {
+			$test_name = $new_name . '-' . $this->get_random_characters( 10, 20 );
+
+			if ( ! is_dir( "$directory/$test_name" ) ) {
+				$new_name = $test_name;
+				break;
+			}
+		}
+
+
+		if ( is_dir( "$directory/$new_name" ) ) {
+			// We gave it our best effort. Time to give up on the idea of having a backup.
+			$this->skin->error( __( 'Unable to find a new directory name to move the old version of the theme to. No backup will be created.', 'easy-theme-and-plugin-upgrades' ) );
+		} else {
+			$result = rename( $destination, "$directory/$new_name" );
+
+			if ( $result ) {
+				/* translators: 1: new theme directory name */
+				$this->skin->feedback( sprintf( __( 'Moved the old version of the theme to a new theme directory named %1$s. This directory should be backed up and removed from the site.', 'easy-theme-and-plugin-upgrades' ), "<code>$new_name</code>" ) );
+			} else {
+				$this->skin->error( __( 'Unable to move the old version of the theme to a new directory. No backup will be created.', 'easy-theme-and-plugin-upgrades' ) );
+			}
+		}
+
+		// Restore default strings and display the original remove_old message.
+		$this->upgrade_strings();
+		$this->skin->feedback( 'remove_old' );
+
+		return parent::clear_destination( $destination );
+	}
+
+	private function create_backup( $directory ) {
+		require_once( ABSPATH . 'wp-admin/includes/file.php' );
+
+		$wp_upload_dir = wp_upload_dir();
+
+		$zip_path = $wp_upload_dir['path'];
+		$zip_url  = $wp_upload_dir['url'];
+
+		if ( ! is_dir( $zip_path ) ) {
+			return new WP_Error( 'caj-etpu-cannot-backup-no-destination-path', __( 'A theme backup can not be created since a destination path for the backup file could not be found.', 'easy-theme-and-plugin-upgrades' ) );
+		}
+
+
+		$headers = array(
+			'name'    => 'Theme Name',
+			'version' => 'Version',
+		);
+		$data = get_file_data( "$directory/style.css", $headers );
+
+		$rand_string = $this->get_random_characters( 10, 20 );
+		$zip_file = basename( $directory ) . "-{$data['version']}-$rand_string.zip";
+
+
+		// Reduce the chance that a timeout will occur while creating the zip file.
+		@set_time_limit( 300 );
+
+
+		$zip_path .= "/$zip_file";
+		$zip_url  .= "/$zip_file";
+
+		$archive = new PclZip( $zip_path );
+
+		$zip_result = $archive->create( $directory, PCLZIP_OPT_REMOVE_PATH, dirname( $directory ) );
+
+		if ( 0 === $zip_result ) {
+			/* translators: 1: zip error details */
+			return new WP_Error( 'caj-etpu-cannot-backup-zip-failed', sprintf( __( 'A theme backup can not be created as creation of the zip file failed with the following error: %1$s', 'easy-theme-and-plugin-upgrades' ), $archive->errorInfo( true ) ) );
+		}
+
+
+		$attachment = array(
+			'post_mime_type' => 'application/zip',
+			'guid'           => $zip_url,
+			/* translators: 1: theme name, 2: theme version */
+			'post_title'     => sprintf( __( 'Theme Backup - %1$s - %2$s', 'easy-theme-and-plugin-upgrades' ), $data['name'], $data['version'] ),
+			'post_content'   => '',
+		);
+
+		$id = wp_insert_attachment( $attachment, $zip_path );
+
+		if ( ! is_wp_error( $id ) ) {
+			wp_update_attachment_metadata( $id, wp_generate_attachment_metadata( $id, $zip_path ) );
+		}
+
+
+		return $zip_url;
+	}
+
+	private function get_random_characters( $min_length, $max_length ) {
+		$characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+		$rand_string = '';
+		$length = rand( $min_length, $max_length );
+
+		for ( $count = 0; $count < $length; $count++ ) {
+			$rand_string .= $characters[rand( 0, strlen( $characters ) - 1 )];
+		}
+
+		return $rand_string;
 	}
 }
--- readme.txt
+++ readme.txt
@@ -3,19 +3,21 @@ Contributors: chrisjean
 Tags: plugin, theme, upgrade, update, upload
 Requires at least: 4.4
 Tested up to: 4.5.3
-Stable tag: 1.0.6
+Stable tag: 2.0.0-dev
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 Easily upgrade your themes and plugins using zip files without removing the theme or plugin first.
 
+
 == Description ==
 
 WordPress has a built-in feature to install themes and plugins by supplying a zip file. Unfortunately, you cannot upgrade a theme or plugin using the same process. Instead, WordPress will say "destination already exists" when trying to upgrade using a zip file and will fail to upgrade the theme or plugin.
 
-Easy Theme and Plugin Upgrades fixes this limitation in WordPress. When active, you get an option to allow the theme or plugin to be upgraded.
+Easy Theme and Plugin Upgrades fixes this limitation in WordPress by automatically upgrading the theme or plugin if it already exists.
 
 While upgrading, a backup copy of the old theme or plugin is first created. This allows you to install the old version in case of problems with the new version.
+
 
 == Frequently Asked Questions ==
 
@@ -40,6 +42,16 @@ While upgrading, a backup copy of the old theme or plugin is first created. This
 1. Select "Yes" from the "Upgrade existing plugin?" option.
 1. Click the "Install Now" button.
 
+= How do I access the backup of an old theme or plugin? =
+
+1. Log into your WordPress site.
+1. Go to Media > Library.
+1. Type "backup" into the search input and press the "Enter" key.
+1. Find the desired backup from the resulting list.
+1. Click the title of the desired backup.
+1. The URL to the backup file is listed on the right side of the page under "File URL". You can copy and paste that URL into your browser's URL bar in order to start a download.
+
+
 == Installation ==
 
 1. Download and unzip the latest release zip file
@@ -47,6 +59,12 @@ While upgrading, a backup copy of the old theme or plugin is first created. This
 1. Activate the plugin through the 'Plugins' menu in WordPress
 
 == Changelog ==
+
+= 2.0.0 =
+ * Enhancement: Removed the requirement for the user to select "Yes" from the drop down in order to initiate an update. This new version does not change the appearance of the upload form. Instead, it automatically creates a backup and performs an upgrade if the supplied plugin or theme already exists.
+ * Enhancement: If a zip backup file cannot be created, the old directory is renamed to a new name in order to still keep a backup.
+ * Enhancement: Updated the code to use a better way of integrating the upgrade logic. This approach should greatly reduce the potential for conflicts with other code or site configurations.
+ * Enhancement: The backup details now are found in the same format as the rest of the upgrade messages.
 
 = 1.0.6 =
  * Bug Fix: Fixed updates not working with some formats of zip files. Thanks to the team at [Kaira](https://kairaweb.com/) for helping solve this issue.
@@ -69,7 +87,8 @@ While upgrading, a backup copy of the old theme or plugin is first created. This
 = 1.0.0 =
  * Initial release version
 
+
 == Upgrade Notice ==
 
-= 1.0.6 =
-Version 1.0.6 contains a bug fix that fixes a zip compatibility issue that many users have reported.
+= 2.0.0 =
+Version 2.0.0 fixes many of the site and plugin compatibility issues that people have reported and removes any need to tell the plugin to perform an upgrade.
